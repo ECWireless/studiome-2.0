ab463d041c87800a394dcb6bd8e7d73f4b19c90d
ECWireless <elliott@coopallc.com>
2023-05-18 12:08:29 -0600

    Add new hero banner and testimonial section

--- src/Pages/Home/Examples.js
+++ src/Pages/Home/Examples.js
@@ -12,56 +12,109 @@ import { H2 } from '../../components/Typography'
 
 const Examples = () => {
 	return (
-		<ExamplesBackground>
-			<Container>
-				<H2 uppercase center>Some of our work</H2>
-				<Box3 marginTop={10} marginBottom={50}>
-						<Line width={150} height={3} color={'orange'} />
-				</Box3>
-				<Flex wrap={true}>
-					<Fade bottom ssrFadeout>
-						<Box3 marginBottom={25}>
-							<CardContainer>
-								<ExampleVideo
-									src={'https://player.vimeo.com/video/651689605'}
-									frameborder={0} allow={'autoplay; fullscreen'} allowfullscreen
-								/>
-							</CardContainer>
-						</Box3>
-					</Fade>
-					<Fade bottom delay={200} ssrFadeout>
-						<Box3 marginBottom={25}>
-							<CardContainer>
-								<ExampleVideo
-									src={'https://player.vimeo.com/video/786112487?h=63d9f4dcad'}
-									frameborder={0} allow={'autoplay; fullscreen'} allowfullscreen
-								/>
-							</CardContainer>
-						</Box3>
-					</Fade>
-					<Fade bottom ssrFadeout>
-						<Box3 marginBottom={25}>
-							<CardContainer>
-								<ExampleVideo
-									src={'https://player.vimeo.com/video/678736360?h=eb055f50d0'}
-									frameborder={0} allow={'autoplay; fullscreen'} allowfullscreen
-								/>
-							</CardContainer>
-						</Box3>
-					</Fade>
-					<Fade bottom delay={200} ssrFadeout>
-						<Box3 marginBottom={25}>
-							<CardContainer>
-								<ExampleVideo
-									src={'https://player.vimeo.com/video/799646886?h=5844db8734'}
-									frameborder={0} allow={'autoplay; fullscreen'} allowfullscreen
-								/>
-							</CardContainer>
-						</Box3>
-					</Fade>
-				</Flex>
-			</Container>
-		</ExamplesBackground>
+		<>
+			<ExamplesBackground>
+				<Container>
+					<H2 uppercase center>Some of our work</H2>
+					<Box3 marginTop={10} marginBottom={50}>
+							<Line width={150} height={3} color={'orange'} />
+					</Box3>
+					<Flex wrap={true}>
+						<Fade bottom ssrFadeout>
+							<Box3 marginBottom={25}>
+								<CardContainer>
+									<ExampleVideo
+										src={'https://player.vimeo.com/video/651689605'}
+										frameborder={0} allow={'autoplay; fullscreen'} allowfullscreen
+									/>
+								</CardContainer>
+							</Box3>
+						</Fade>
+						<Fade bottom delay={200} ssrFadeout>
+							<Box3 marginBottom={25}>
+								<CardContainer>
+									<ExampleVideo
+										src={'https://player.vimeo.com/video/786112487?h=63d9f4dcad'}
+										frameborder={0} allow={'autoplay; fullscreen'} allowfullscreen
+									/>
+								</CardContainer>
+							</Box3>
+						</Fade>
+						<Fade bottom ssrFadeout>
+							<Box3 marginBottom={25}>
+								<CardContainer>
+									<ExampleVideo
+										src={'https://player.vimeo.com/video/678736360?h=eb055f50d0'}
+										frameborder={0} allow={'autoplay; fullscreen'} allowfullscreen
+									/>
+								</CardContainer>
+							</Box3>
+						</Fade>
+						<Fade bottom delay={200} ssrFadeout>
+							<Box3 marginBottom={25}>
+								<CardContainer>
+									<ExampleVideo
+										src={'https://player.vimeo.com/video/799646886?h=5844db8734'}
+										frameborder={0} allow={'autoplay; fullscreen'} allowfullscreen
+									/>
+								</CardContainer>
+							</Box3>
+						</Fade>
+					</Flex>
+				</Container>
+				</ExamplesBackground>
+
+			<ExamplesBackground>
+				<Container>
+					<H2 uppercase center>What Our Clients Say</H2>
+					<Box3 marginTop={10} marginBottom={50}>
+							<Line width={150} height={3} color={'orange'} />
+					</Box3>
+					<Flex wrap={true}>
+						<Fade bottom ssrFadeout>
+							<Box3 marginBottom={25}>
+								<CardContainer>
+									<ExampleVideo
+										src={'https://player.vimeo.com/video/794582308?h=7d76898675'}
+										frameborder={0} allow={'autoplay; fullscreen'} allowfullscreen
+									/>
+								</CardContainer>
+							</Box3>
+						</Fade>
+						<Fade bottom delay={200} ssrFadeout>
+							<Box3 marginBottom={25}>
+								<CardContainer>
+									<ExampleVideo
+										src={'https://player.vimeo.com/video/495195149?h=96a76e7e52'}
+										frameborder={0} allow={'autoplay; fullscreen'} allowfullscreen
+									/>
+								</CardContainer>
+							</Box3>
+						</Fade>
+						<Fade bottom ssrFadeout>
+							<Box3 marginBottom={25}>
+								<CardContainer>
+									<ExampleVideo
+										src={'https://player.vimeo.com/video/822105710?h=9ea971081e'}
+										frameborder={0} allow={'autoplay; fullscreen'} allowfullscreen
+									/>
+								</CardContainer>
+							</Box3>
+						</Fade>
+						<Fade bottom delay={200} ssrFadeout>
+							<Box3 marginBottom={25}>
+								<CardContainer>
+									<ExampleVideo
+										src={'https://player.vimeo.com/video/284189590?h=0dff5455fa'}
+										frameborder={0} allow={'autoplay; fullscreen'} allowfullscreen
+									/>
+								</CardContainer>
+							</Box3>
+						</Fade>
+						</Flex>
+					</Container>
+				</ExamplesBackground>
+			</>
 	)
 }
 
--- src/Pages/Home/Hero.js
+++ src/Pages/Home/Hero.js
@@ -1,63 +1,82 @@
 import React, { Component } from 'react'
 import Lottie from 'react-lottie';
+import styled from 'styled-components';
+import respondTo from '../../components/Breakpoints';
 
-// Components
+
 import Spinner from '../../components/Spinner';
 
-// Video
 import webVideo from '../../assets/home/hero/web-hero-4.mp4';
 
-// SVGs
 import { ReactComponent as ChevronArrow } from '../../assets/home/products/chevron-thin-down.svg';
 import videoSVG from '../../assets/home/hero/video.json';
+import FiveStar from '../../assets/home/hero/five-star.png';
 
 export default class Hero extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {isStopped: false, isPaused: false};
-    }
-    
-    render() {
-        const defaultOptions = {
-            loop: true,
-            autoplay: true, 
-            animationData: videoSVG,
-            rendererSettings: {
-                preserveAspectRatio: 'xMidYMid slice'
-            }
-        };
+  constructor(props) {
+    super(props);
+    this.state = {isStopped: false, isPaused: false};
+  }
+  
+  render() {
+    const defaultOptions = {
+      loop: true,
+      autoplay: true, 
+      animationData: videoSVG,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+      }
+    };
 
-        return (
-            <section id="hero" className="container">
-                <React.Suspense fallback={<Spinner />}>
-                    <div id="hero__video-container">
-                        <video src={webVideo} id="hero__video" autoPlay loop muted playsInline />
-                    </div>
-                </React.Suspense>
+    return (
+      <section id="hero" className="container">
+        <React.Suspense fallback={<Spinner />}>
+          <div id="hero__video-container">
+            <video src={webVideo} id="hero__video" autoPlay loop muted playsInline />
+            <RatingBadgeContainer>
+              <img src={FiveStar} alt="Five Star Rating" />
+            </RatingBadgeContainer>
+          </div>
+        </React.Suspense>
 
-                <div id="hero__video-svg">
-                    <Lottie options={defaultOptions}
-                        isStopped={this.state.isStopped}
-                        isPaused={this.state.isPaused}
-                    />
-                </div>
+        <div id="hero__video-svg">
+          <Lottie options={defaultOptions}
+            isStopped={this.state.isStopped}
+            isPaused={this.state.isPaused}
+          />
+        </div>
 
-                <div id="hero__circle" />
-                
-                <div id="hero__headings-container">
-                    <h1 id="hero__heading" className="h1 h1 white">{this.props.heroMobileTagline}</h1>
-                    <h3 id="hero__subheading" className="h3 h3 white l">{this.props.heroMobileDescription}</h3>
-                </div>
+        <div id="hero__circle" />
+        
+        <div id="hero__headings-container">
+          <h1 id="hero__heading" className="h1 h1 white">{this.props.heroMobileTagline}</h1>
+          <h3 id="hero__subheading" className="h3 h3 white l">{this.props.heroMobileDescription}</h3>
+        </div>
 
-                <div id="hero__description-container" className="white">
-                    <h2 id="hero__studio-description" className="h2 h2 white">{this.props.heroDesktopDescription}</h2>
-                </div>
+        <div id="hero__description-container" className="white">
+          <h2 id="hero__studio-description" className="h2 h2 white">{this.props.heroDesktopDescription}</h2>
+        </div>
 
-                <div onClick={this.props.onProductsHandler} id="hero__arrow-container">
-                    <h3 id="hero__arrow-heading" className="h3 l">{this.props.heroProductsIndicatorText}</h3>
-                    <ChevronArrow id="hero__arrow" />
-                </div>
-            </section>
-        )
-    }
+        <div onClick={this.props.onProductsHandler} id="hero__arrow-container">
+          <h3 id="hero__arrow-heading" className="h3 l">{this.props.heroProductsIndicatorText}</h3>
+          <ChevronArrow id="hero__arrow" />
+        </div>
+      </section>
+    )
+  }
 }
+
+const RatingBadgeContainer = styled.div`
+  display: none;
+  position: absolute;
+  right: 0;
+  top: 60px;
+
+  ${respondTo.md`
+    display: block;
+  `}
+
+  img {
+    width: 300px;
+  }
+`;
